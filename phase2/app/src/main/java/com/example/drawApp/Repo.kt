3598a2ce2a2cec8e6e36d36efe15c22afe56e670
Olivia <olivia.chengman@gmail.com>
2023-10-23 23:47:32 -0600

    report

--- phase2/app/src/main/java/com/example/drawApp/Repo.kt
+++ phase2/app/src/main/java/com/example/drawApp/Repo.kt
@@ -1,4 +1,8 @@
 package com.example.drawApp
 
+import androidx.lifecycle.LiveData
+//add this interface to mock call DrawingRepo because it's not easy to directly mock a class
 interface Repo {
+    val allDrawings: LiveData<List<DrawData>>
+    fun addDrawingToDB(filename: String, filepath: String)
 }
