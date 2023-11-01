308648a6e48127f2e8f3c2191887364903343c1a
Jeffery18-hub <ljzmcs@gmail.com>
2023-11-01 11:40:30 -0600

    add server ktor basics

---
+++ server/lab6_ktor/src/main/kotlin/com/example/Models.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.timestamp
+import java.sql.Timestamp
+
+
+object Post: IntIdTable(){ // object for singleton table
+    val imageName = varchar("name", 255)
+    val imagePath = varchar("path", 255)
+    val uid = varchar("uid", 255)
+    val postTime = long("postTime")
+}
